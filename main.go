0b4968dd8ccc70651cc9acc8caa0fd174d7d03ea
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2024-05-25 21:29:34 +0900

    add sample main file

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.GET("/", func(c echo.Context) error {
+		return c.String(200, "Hello, World!")
+	})
+
+	e.Start(":8080")
+}
