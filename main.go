da9935b6b1b692367d3eefd96153851924179090
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2024-05-25 21:47:17 +0900

    use sqlite3 in sample code

--- main.go
+++ main.go
@@ -1,16 +1,67 @@
 package main
 
+import "C"
 import (
+	"database/sql"
+	"log"
+	"net/http"
+	"strconv"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	_ "github.com/mattn/go-sqlite3"
 )
 
-func main() {
-	e := echo.New()
+type User struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
 
+func initDB(filepath string) *sql.DB {
+	db, err := sql.Open("sqlite3", filepath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return db
+}
+
+func main() {
+	db := initDB("example.db")
+	e := echo.New()
 	e.Use(middleware.Logger())
-	e.GET("/", func(c echo.Context) error {
-		return c.String(200, "Hello, World!")
+
+	e.POST("/users", func(c echo.Context) error {
+		name := c.FormValue("name")
+		age, _ := strconv.Atoi(c.FormValue("age"))
+
+		result, err := db.Exec("INSERT INTO users (name, age) VALUES (?, ?)", name, age)
+		if err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+		}
+
+		id, _ := result.LastInsertId()
+
+		return c.JSON(http.StatusOK, &User{ID: int(id), Name: name, Age: age})
+	})
+
+	e.GET("/users", func(c echo.Context) error {
+		rows, err := db.Query("SELECT id, name, age FROM users")
+		if err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+		}
+		defer rows.Close()
+
+		users := []User{}
+		for rows.Next() {
+			var user User
+			if err := rows.Scan(&user.ID, &user.Name, &user.Age); err != nil {
+				return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+			}
+			users = append(users, user)
+		}
+
+		return c.JSON(http.StatusOK, users)
 	})
 
 	e.Start(":8080")
